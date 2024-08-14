69917ab54827f45d59be57e2ad89311533d326e7
Kamil <e-kamil97-97@mail.ru>
2024-08-14 13:16:58 +0300

    add count test

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -4,6 +4,12 @@ import './Pagination.module.scss';
 export const Pagination = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [items, setItems] = useState([]);
+  const [count, setCount] = useState(0);
 
-  return <h1>Pagination</h1>;
+  return (
+    <>
+      <h2>{count}</h2>
+      <button onClick={() => setCount(count + 1)}>Click</button>
+    </>
+  );
 };
