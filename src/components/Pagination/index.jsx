b12e9964faf676f2ae485e9557dfc6c68e12846a
Kamil <e-kamil97-97@mail.ru>
2024-08-14 12:40:37 +0300

    added new branch

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Pagination.module.scss';
 
 export const Pagination = () => {
-  return <div>Pagination</div>;
+  const [currentPage, setCurrentPage] = useState(1);
+  const [items, setItems] = useState([]);
+
+  return <h1>Pagination</h1>;
 };
